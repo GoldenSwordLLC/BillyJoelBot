f548b267ba27fbf1a69c07cd3aa3452530ebdf08
Lvl4Sword <17956230+Lvl4Sword@users.noreply.github.com>
2023-10-19 00:59:05 -0600

    Remove repetivie code, add for loops

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discord
 import pyaudio
 from config import super_secret_token
 
+# sword, party
+the_channels = [1151755897366904914, 1151092359048724544]
+
 
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
@@ -11,7 +14,6 @@ class MyClient(discord.Client):
         self.p = None
         self.SPEAKERS = None
         self.stream = None
-        self.in_voice = False
 
     def setup_pyaudio(self):
         self.p = pyaudio.PyAudio()
@@ -25,93 +27,52 @@ class MyClient(discord.Client):
                                   input_device_index=self.SPEAKERS)
 
     async def on_ready(self, member):
-        party_channel_id = 1151092359048724544
-        party_channel = client.get_channel(party_channel_id)
-        sword_channel_id = 1151755897366904914
-        sword_channel = client.get_channel(sword_channel_id)
-        if isinstance(party_channel, discord.VoiceChannel):
-            if len(party_channel.members) == 0:
-                pass
-            elif len(party_channel.members) >= 1:
-                found_bot = False
-                for each_user in party_channel.members:
-                    if each_user.id == 1151658242108305539:
-                        found_bot = True
-                if not found_bot:
-                    await party_channel.connect()
-                    self.in_voice = True
-                else:
-                    voice = member.guild.voice_client
-                    await voice.disconnect(force=True)
-                    self.in_voice = False
-        if isinstance(sword_channel, discord.VoiceChannel):
-            if len(sword_channel.members) == 0:
-                pass
-            elif len(sword_channel.members) >= 1:
-                found_bot = False
-                for each_user in sword_channel.members:
-                    if each_user.id == 1151658242108305539:
-                        found_bot = True
-                if not found_bot:
-                    await sword_channel.connect()
-                    self.in_voice = True
-                else:
-                    voice = member.guild.voice_client
-                    await voice.disconnect(force=True)
-                    self.in_voice = False
-
-    # async def close(self):
-    #    await client.close()
-    #    print("Billy Joel has disconnected.")
+        for each_channel in the_channels:
+            try:
+                each_channel = int(each_channel)
+            except ValueError:
+                print(f'ERROR: {each_channel} is not a channel integer.')
+            else:
+                this_channel = client.get_channel(each_channel)
+                if isinstance(this_channel, discord.VoiceChannel):
+                    if len(this_channel.members) == 0:
+                        pass
+                    elif len(this_channel.members) >= 1:
+                        found_bot = False
+                        for each_user in this_channel.members:
+                            if each_user.id == 1151658242108305539:
+                                found_bot = True
+                        if not found_bot:
+                            await this_channel.connect()
+                        else:
+                            voice = member.guild.voice_client
+                            await voice.disconnect(force=True)
 
     async def on_voice_state_update(self, member, before, after):
         found_bot = False
-        party_channel_id = 1151092359048724544
-        party_channel = client.get_channel(party_channel_id)
-        sword_channel_id = 1151755897366904914
-        sword_channel = client.get_channel(sword_channel_id)
-        if isinstance(party_channel, discord.VoiceChannel):
-            if len(party_channel.members) == 0:
-                # We probably just left, so don't do anything
-                pass
-            elif len(party_channel.members) == 1:
-                if after.channel is None:
-                    voice = member.guild.voice_client
-                    await voice.disconnect(force=True)
-                    self.in_voice = False
-                else:
-                    if not self.in_voice:
-                        await sword_channel.connect()
-                        self.in_voice = True
+        for each_channel in the_channels:
+            try:
+                each_channel = int(each_channel)
+            except ValueError:
+                print(f'ERROR: {each_channel} is not a channel integer.')
             else:
-                found_bot = False
-                for each_user in party_channel.members:
+                this_channel = client.get_channel(each_channel)
+                for each_user in this_channel.members:
                     if each_user.id == 1151658242108305539:
                         found_bot = True
-                if not found_bot:
-                    await party_channel.connect()
-                    self.in_voice = True
-        if isinstance(sword_channel, discord.VoiceChannel):
-            if len(sword_channel.members) == 0:
-                # We probably just left, so don't do anything
-                pass
-            elif len(sword_channel.members) == 1:
-                if after.channel is None:
-                    voice = member.guild.voice_client
-                    await voice.disconnect(force=True)
-                    self.in_voice = False
+                if len(this_channel.members) == 0:
+                    # We probably just left, so don't do anything
+                    pass
+                elif len(this_channel.members) == 1:
+                    if after.channel is None:
+                        voice = member.guild.voice_client
+                        await voice.disconnect(force=True)
+                    else:
+                        if not found_bot:
+                            await this_channel.connect()
                 else:
-                    if not self.in_voice:
-                        await sword_channel.connect()
-                        self.in_voice = True
-            else:
-                found_bot = False
-                for each_user in sword_channel.members:
-                    if each_user.id == 1151658242108305539:
-                        found_bot = True
-                if not found_bot:
-                    await sword_channel.connect()
-                    self.in_voice = True
+                    if not found_bot:
+                        await this_channel.connect()
 
     async def play_music(self, member):
         while self.in_voice:
